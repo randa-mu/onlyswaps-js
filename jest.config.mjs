b29ba88db2c2f415521be3d167ecc0cb8240c366
Patrick McClurg <patrick.mcclurg@protocol.ai>
2025-07-11 04:20:47 +0200

    improved the interface, integrated swap and bits to check your request
    - a basic test with anvil integration (run it locally)
    - removed furnace

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,14 +1,14 @@
 export default {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  extensionsToTreatAsEsm: [".ts"],
-  moduleNameMapper: {
-    '^@/solidity/(.*)$': '<rootDir>/onlysubs-solidity/out/$1'
-  },
-  transform: {
-    "^.+\\.(ts|tsx)$": ["ts-jest", {
-      useESM: true,
-    }],
-  },
-  testTimeout: 50000,
+    preset: "ts-jest",
+    testEnvironment: "node",
+    extensionsToTreatAsEsm: [".ts"],
+    moduleNameMapper: {
+        '^@/solidity/(.*)$': '<rootDir>/onlysubs-solidity/out/$1'
+    },
+    transform: {
+        "^.+\\.(ts|tsx)$": ["ts-jest", {
+            useESM: true,
+        }],
+    },
+    testTimeout: 50000,
 };
--- test/swap.test.ts
+++ test/swap.test.ts
@@ -1,8 +1,40 @@
-import { OnlySwaps } from "../src"
-import { test } from "@jest/globals"
+import { expect, test } from "@jest/globals"
+import { Abi, createPublicClient, createWalletClient, http } from "viem"
+import { foundry } from "viem/chains"
+import { privateKeyToAccount } from "viem/accounts"
+import { OnlySwapsViemClient } from "../src"
+import RouterJson from "../onlysubs-solidity/out/Router.sol/Router.json"
+
+const account = privateKeyToAccount("0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80")
+const publicClient = createPublicClient({
+    chain: { ...foundry, id: 1337 },
+    transport: http("http://localhost:1337"),
+})
+
+const walletClient = createWalletClient({
+    chain: { ...foundry, id: 1337 },
+    transport: http("http://localhost:1337"),
+    account,
+})
+
+const RUSD_ADDRESS = "0x46D346f8d9582f8963110108A7988B1a0bB3668D"
 
 test("swap resolve", async () => {
-  const onlyswaps = new OnlySwaps()
-  await onlyswaps.swap({ amount: 1n, sourceChain: 1n, destinationChain: 2n })
+    const client = new OnlySwapsViemClient(
+        "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
+        { abi: RouterJson.abi as Abi , address: "0xD10fdc7B6E049Ee482a1C202dB996eC4fFA36370" },
+        publicClient,
+        walletClient
+    )
+
+    const { requestId } = await client.swap({
+        recipient: "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
+        tokenAddress: RUSD_ADDRESS,
+        amount: 100n,
+        fee: 1n,
+        destinationChainId: 1338n
+    })
+
+    expect(requestId).not.toBe(undefined)
 })
 
