75105acbe152223323b5a583a893f66be742ca5b
Robin Müller <33422394+azixus@users.noreply.github.com>
2025-09-25 12:47:51 +0530

    fix(eth): take a PublicActions type instead of PublicClient in throwOnError

--- src/eth.ts
+++ src/eth.ts
@@ -1,8 +1,8 @@
-import { Abi, decodeErrorResult, PublicClient, TransactionReceipt } from "viem"
+import { Abi, decodeErrorResult, PublicActions, TransactionReceipt } from "viem"
 
 // throwOnError will do some magical parsing and simulation to get a useful error message
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export async function throwOnError(receipt: TransactionReceipt, abi: Abi, client: PublicClient, params: any) {
+export async function throwOnError(receipt: TransactionReceipt, abi: Abi, client: PublicActions, params: any) {
     if (receipt.status === "success") {
         return
     }
