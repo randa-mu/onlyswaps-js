444d25e103fa6af65904f6e4fad6236f64cb39d8
Robin Müller <33422394+azixus@users.noreply.github.com>
2025-09-25 12:50:42 +0530

    fix(rusd): allow calling `RUSDViemClient` with a read-only client, require a wallet client for write functions

--- src/rusd.ts
+++ src/rusd.ts
@@ -1,8 +1,9 @@
-import { Abi, Address, PublicClient, WalletClient } from "viem"
+import { Abi, Address } from "viem"
 import { waitForTransactionReceipt } from "viem/actions"
 import ERC20FaucetToken from "../onlysubs-solidity/out/ERC20FaucetToken.sol/ERC20FaucetToken.json"
 import { throwOnError } from "./eth"
 import Decimal from "decimal.js"
+import { OSPublicClient, OSWalletClient } from "./model"
 
 const DEFAULT_ABI: Abi = ERC20FaucetToken.abi as Abi
 
@@ -14,32 +15,29 @@ export interface RUSD {
     approveSpend(address: Address, amount: bigint): Promise<void>
 }
 
-export class RUSDViemClient implements RUSD {
-
+export class RUSDViemClient<client extends OSPublicClient> implements RUSD {
     constructor(
         private account: Address,
         private contractAddr: Address,
-        private publicClient: PublicClient,
-        private walletClient: WalletClient,
+        private client: client,
         private abi: Abi = DEFAULT_ABI
     ) {
     }
 
-    async mint(): Promise<void> {
-        const hash = await this.walletClient.writeContract({
+    async mint(this: RUSDViemClient<client & OSWalletClient>): Promise<void> {
+        const hash = await this.client.writeContract({
             functionName: "mint",
             abi: this.abi,
             address: this.contractAddr,
-            account: this.account,
-            chain: this.walletClient.chain,
+            chain: this.client.chain,
             args: [],
         })
 
-        await waitForTransactionReceipt(this.walletClient, { hash })
+        await waitForTransactionReceipt(this.client, { hash })
     }
 
     async balanceOf(address: Address): Promise<bigint> {
-        const response = await this.publicClient.readContract({
+        const response = await this.client.readContract({
             abi: this.abi,
             address: this.contractAddr,
             functionName: "balanceOf",
@@ -48,19 +46,18 @@ export class RUSDViemClient implements RUSD {
         return response as bigint
     }
 
-    async approveSpend(address: Address, amount: bigint): Promise<void> {
+    async approveSpend(this: RUSDViemClient<client & OSWalletClient>, address: Address, amount: bigint): Promise<void> {
         const approvalParams = {
             functionName: "approve",
             abi: this.abi,
             address: this.contractAddr,
-            account: this.account,
-            chain: this.walletClient.chain,
+            chain: this.client.chain,
             args: [address, amount]
         }
-        const hash = await this.walletClient.writeContract(approvalParams)
+        const hash = await this.client.writeContract(approvalParams)
 
-        const receipt = await waitForTransactionReceipt(this.walletClient, { hash })
-        await throwOnError(receipt, this.abi, this.publicClient, approvalParams)
+        const receipt = await waitForTransactionReceipt(this.client, { hash })
+        await throwOnError(receipt, this.abi, this.client, approvalParams)
     }
 
 }
